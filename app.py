c468ef0c15b9c4399f0b7830bd3078a9c7fd1dfd
graceduansu <54904766+graceduansu@users.noreply.github.com>
2019-11-09 10:01:23 -0500

    automatically generate topic pages

--- app.py
+++ app.py
@@ -8,44 +8,45 @@ newsapi = NewsApiClient(api_key='466f16c5dc2445eabe6a30991514a281')
 @app.route('/')
 def main():
     articles = []
-    impeachment_articles = newsapi.get_top_headlines(q='impeachment', language='en',
-                                                     page_size=1).get('articles')
-    vaping_articles = newsapi.get_top_headlines(q='politics', language='en', page_size=1).get('articles')
-    election_articles = newsapi.get_top_headlines(q='election', language='en', page_size=1).get('articles')
-    hong_kong_articles = newsapi.get_top_headlines(q='hong kong', language='en', page_size=1).get('articles')
-    articles.append(impeachment_articles)
-    articles.append(vaping_articles)
-    articles.append(election_articles)
-    articles.append(hong_kong_articles)
+    top_topics = ['Impeachment', 'Vaping', 'Election', 'Hong Kong']
+    for topic in top_topics:
+        articles.append(get_topic(topic))
     print(articles)
-    return render_template('index.html', articles_list=articles)
-
-@app.route('/topic')
-def topic():
-	return render_template('topic.html', );
+    return render_template('index.html', articles_list=articles, topics_list=top_topics)
 
 
-@app.route('/news-source-stats<query>', methods=['GET'])
+@app.route('/topic_page/<topic_name>')
+def render_topic(topic_name):
+    topic_articles = get_topic(topic_name)
+    return render_template('topic.html', topic=topic_name, articles_list=topic_articles)
+
+
+@app.route('/topic/<topic_name>')
+def get_topic(topic_name):
+    return newsapi.get_top_headlines(q=topic_name, language='en', page_size=1).get('articles')
+
+
+@app.route('/news-source-stats/<query>', methods=['GET'])
 def get_news_source_stats(query):
     return
 
 
-@app.route('/votes<query>', methods=['GET'])
+@app.route('/votes/<query>', methods=['GET'])
 def get_votes(query):
     return
 
 
-@app.route('/target-audience<query>', methods=['GET'])
+@app.route('/target-audience/<query>', methods=['GET'])
 def get_target_audience(query):
     return
 
 
-@app.route('/locality<query>', methods=['GET'])
+@app.route('/locality/<query>', methods=['GET'])
 def get_locality(query):
     return
 
 
-@app.route('/reporting-topics<query>', methods=['GET'])
+@app.route('/reporting-topics/<query>', methods=['GET'])
 def get_reporting_topics(query):
     return
 
